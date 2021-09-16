da7b8a61a15084145228295c9034c54bc5eb0f88
Gzy <854606081@qq.com>
2021-09-16 12:32:58 +0800

    v1.0——基于BIO与动态代理

--- test-client/src/main/java/cn/tjgzy/myrpc/TestClient.java
+++ test-client/src/main/java/cn/tjgzy/myrpc/TestClient.java
@@ -2,6 +2,8 @@ package cn.tjgzy.myrpc;
 
 import cn.tjgzy.myrpc.client.RpcClientProxy;
 
+import java.lang.reflect.Proxy;
+
 /**
  * @author GongZheyi
  * @create 2021-09-16-12:12
@@ -10,7 +12,9 @@ public class TestClient {
     public static void main(String[] args) {
         RpcClientProxy rpcClientProxy = new RpcClientProxy("127.0.0.1",8889);
         // 根据HelloService获取一个代理对象，执行时会调用invoke方法发送RpcRequest
-        HelloService helloService = rpcClientProxy.getProxy(HelloService.class);
+        HelloService helloService = (HelloService) Proxy.newProxyInstance(HelloService.class.getClassLoader(),
+                new Class<?>[]{HelloService.class}, rpcClientProxy);
+//        HelloService helloService = rpcClientProxy.getProxy(HelloService.class);
         HelloObject object = new HelloObject(12, "This is a message");
         String s = helloService.hello(object);
         System.out.println(s);
